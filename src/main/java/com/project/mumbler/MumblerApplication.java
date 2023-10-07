8b0d51c6ee9f9e1feec0b5be9d1aa6e304d443be
J0SEOYOUNG <loveliest@cau.ac.kr>
2023-10-07 16:27:14 +0900

    first commit

---
+++ src/main/java/com/project/mumbler/MumblerApplication.java
@@ -0,0 +1,13 @@
+package com.project.mumbler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumblerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MumblerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/mumbler/MumblerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mumbler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MumblerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
